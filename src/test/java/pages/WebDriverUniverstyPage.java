f227725310290070d0e086393e27faeaece491df
aliklc44 <101723891+aliklc44@users.noreply.github.com>
2022-06-05 17:27:33 +0300

    yeni

---
+++ src/test/java/pages/WebDriverUniverstyPage.java
@@ -0,0 +1,25 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class WebDriverUniverstyPage {
+    public WebDriverUniverstyPage(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+
+    }
+
+    @FindBy(id = "button1")
+    public WebElement javaScriptAlert;
+
+    @FindBy(xpath = "//span[@id='button4']")
+    public WebElement javaScriptConfirmBox;
+
+    @FindBy(xpath = "//p[@id='confirm-alert-text']")
+    public WebElement yaziElementi;
+
+
+}
